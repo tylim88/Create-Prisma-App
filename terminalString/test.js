f21cc51890b41f4f3bb893545431190d5e70aa2c
unknown <limteikyee@gmail.com>
2018-11-26 23:10:38 +0800

    add npm scripts, psql is now hosted on amazon, no longer in docker due to complicated technical issue

--- terminalString/test.js
+++ terminalString/test.js
@@ -2,6 +2,6 @@ const chalk = require('chalk')
 
 console.log(
   chalk.bgYellow.rgb(0, 0, 0)(
-    'Test environment launching in 12 seconds, you will be able to create and query users soon!'
+    'Test environment launching in few seconds, you will be able to create and query users soon!'
   )
 )
